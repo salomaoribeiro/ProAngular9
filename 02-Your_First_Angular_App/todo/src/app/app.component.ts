119064ae489b7387a6a529e1b8255b948d07a668
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-13 18:16:19 -0300

    Todo project created

---
+++ 02-Your_First_Angular_App/todo/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { TodoItem } from './todoItem';
+import { TodoList } from './todoList';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+  title = 'todo';
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+  ]);
+
+  get username() : string{
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete == true).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
+  }
+}
---
+++ 02-Your_First_Angular_App/todo/src/app/todoItem.ts
@@ -0,0 +1,10 @@
+export class TodoItem {
+
+  task: string;
+  complete: boolean;
+
+  constructor(taskVal: string, completeVal: boolean = false) {
+    this.task = taskVal;
+    this.complete = completeVal;
+  }
+}
