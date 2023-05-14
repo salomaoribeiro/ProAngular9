a4d51fee3ff3346ebc6489d21d06d2fc85b809ae
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-14 00:47:25 -0300

    add a full view of the tasks - incomplete or all

--- 02-Your_First_Angular_App/todo/src/app/app.component.ts
+++ 02-Your_First_Angular_App/todo/src/app/app.component.ts
@@ -1,31 +1,37 @@
-import { Component } from '@angular/core';
-import { TodoItem } from './todoItem';
-import { TodoList } from './todoList';
+import { Component } from "@angular/core";
+import { TodoItem } from "./todoItem";
+import { TodoList } from "./todoList";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-
-
 export class AppComponent {
-  title = 'todo';
+  title = "Todo";
+  showComplete: boolean = false;
+
   private list = new TodoList("Bob", [
     new TodoItem("Go for run", true),
     new TodoItem("Get flowers"),
     new TodoItem("Collect tickets"),
   ]);
 
-  get username() : string{
+  get username(): string {
     return this.list.user;
   }
 
   get itemCount(): number {
-    return this.list.items.filter(item => !item.complete == true).length;
+    return this.items.length;
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items;
+    return this.list.items.filter(
+      (item) => this.showComplete || !item.complete
+    );
+  }
+
+  public addItem(task: string) {
+    if (task != "") this.list.addItem(task);
   }
 }
