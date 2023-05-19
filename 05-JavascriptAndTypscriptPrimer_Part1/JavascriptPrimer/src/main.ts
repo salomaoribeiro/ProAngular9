464bc0104dc4161b491218f5b976ecf60c66f603
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-19 18:56:45 -0300

    chapter 5 - conditional statements

--- 05-JavascriptAndTypscriptPrimer_Part1/JavascriptPrimer/src/main.ts
+++ 05-JavascriptAndTypscriptPrimer_Part1/JavascriptPrimer/src/main.ts
@@ -46,3 +46,26 @@ let messageFunction = function (weather) {
 };
 
 messageFunction("raining");
+
+let firstName = "Adam";
+if (firstName == "Adam") {
+  console.log("firstName is Adam");
+} else if (firstName == "Jacqui") {
+console.log("firstName is Jacqui");
+} else {
+  console.log("firstName is neither Adam or Jacqui");
+}
+
+switch (firstName) {
+  case "Adam":
+    console.log("firstName is Adam");
+    break;
+  case "Jacqui"
+    console.log("firstName is Jacqui");
+    break;
+  default:
+    console.log("firstName is neither Adam or Jacqui");
+    break;
+}
+
+
