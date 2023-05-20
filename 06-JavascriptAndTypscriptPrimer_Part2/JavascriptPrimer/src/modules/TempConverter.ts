bc2280849f4b14e0fa16bf02ff015bf6f704ee03
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-20 18:58:53 -0300

    chapter 6 - using access modifiers

--- 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/modules/TempConverter.ts
+++ 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/modules/TempConverter.ts
@@ -4,6 +4,10 @@ export class TempConverter {
       ? <number>temp
       : parseFloat(<string>temp);
 
-    return ((parseFloat(value.toPrecision(2)) - 32) / 1.8).toFixed(1);
+    return TempConverter.performCalculation(value).toFixed(1);
+  }
+
+  private static performCalculation(value: number): number {
+    return (parseFloat(value.toPrecision(2)) - 32) / 1.8;
   }
 }
