e7ecb8d38d6e7b49ffb836116d68e1ec4428cef2
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-20 17:39:53 -0300

    chapter 6 - renaming imports

--- 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/main.ts
+++ 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/main.ts
@@ -1,7 +1,11 @@
 import { Name, Weather } from "./modules/NameAndWeather";
+import { Name as OtherName } from "./modules/DuplicatedName";
 
 let name = new Name("Adam", "Smith");
 let weather = new Weather("sunny", "London");
 
+let otherName = new OtherName();
+
 console.log(name.message);
 console.log(weather.message);
+console.log(otherName.message);
