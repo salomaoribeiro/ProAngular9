45da885608ba60854bceed22dac2eacdb850d40a
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-20 18:50:16 -0300

    chapter 6 - using indexable types

--- 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/main.ts
+++ 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/main.ts
@@ -4,12 +4,7 @@ import { TempConverter } from "./modules/TempConverter";
 
 let name = new Name("Adam", "Smith");
 let weather = new Weather("sunny", "London");
-
 let otherName = new OtherName();
-
-let tuple: [string, string, string];
-
-tuple = ["London", "raining", TempConverter.convertFtoC(150)];
 
 console.log(name.message);
 console.log(weather.message);
@@ -18,4 +13,17 @@ console.log(otherName.message);
 console.log(TempConverter.convertFtoC(38));
 console.log(TempConverter.convertFtoC("100"));
 
+let tuple: [string, string, string];
+
+tuple = ["London", "raining", TempConverter.convertFtoC(150)];
 console.log(`It is ${tuple[2]} degrees C and it is ${tuple[1]} in ${tuple[0]}`);
+
+let cities: { [index: string]: [string, string] } = {};
+
+cities["London"] = ["raining", TempConverter.convertFtoC(38)];
+cities["Paris"] = ["sunny", TempConverter.convertFtoC(52)];
+cities["Brasil"] = ["snowing", TempConverter.convertFtoC(138)];
+
+for (let key in cities) {
+  console.log(`${key}: ${cities[key][0]} - ${cities[key][1]}`);
+}
