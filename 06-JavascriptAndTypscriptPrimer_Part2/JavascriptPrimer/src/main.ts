081f10410153c26db9cc59a0e07fc8b5cc69c0bc
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-20 18:38:51 -0300

    chapter 6 - using tuples

--- 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/main.ts
+++ 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/main.ts
@@ -1,10 +1,15 @@
 import { Name, Weather } from "./modules/NameAndWeather";
 import { Name as OtherName } from "./modules/DuplicatedName";
 import { TempConverter } from "./modules/TempConverter";
+
 let name = new Name("Adam", "Smith");
 let weather = new Weather("sunny", "London");
 
 let otherName = new OtherName();
+
+let tuple: [string, string, string];
+
+tuple = ["London", "raining", TempConverter.convertFtoC(150)];
 
 console.log(name.message);
 console.log(weather.message);
@@ -12,3 +17,5 @@ console.log(otherName.message);
 
 console.log(TempConverter.convertFtoC(38));
 console.log(TempConverter.convertFtoC("100"));
+
+console.log(`It is ${tuple[2]} degrees C and it is ${tuple[1]} in ${tuple[0]}`);
