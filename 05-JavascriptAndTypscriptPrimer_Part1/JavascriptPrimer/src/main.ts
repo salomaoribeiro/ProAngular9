f79d3606d653b40d3dfac4ead5415b7e198356d4
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-19 22:44:22 -0300

    chapter 5 - using built-in array methods

--- 05-JavascriptAndTypscriptPrimer_Part1/JavascriptPrimer/src/main.ts
+++ 05-JavascriptAndTypscriptPrimer_Part1/JavascriptPrimer/src/main.ts
@@ -110,3 +110,16 @@ let myArray3 = [...myArray2, 200, "Bob", false];
 for (let index = 0; index < myArray3.length; index++) {
   console.log(`Array item: ${index} : ${myArray3[index]}`);
 }
+
+let products = [
+  { name: "Hat", price: 24.5, stock: 10 },
+  { name: "kayak", price: 289.99, stock: 1 },
+  { name: "Soccer Ball", price: 10, stock: 0 },
+  { name: "Running Shoes", price: 116.5, stock: 20 },
+];
+
+let totalValues = products
+  .filter((item) => item.price > 0)
+  .reduce((prev, item) => prev + item.price * item.stock, 0);
+
+console.log("Total value: $" + totalValues.toFixed(2));
