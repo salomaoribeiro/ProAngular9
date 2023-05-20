4e0a81d588f23441397c49d986d6d97cf8ef9bd8
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-20 17:55:41 -0300

    chapter 6 - using type annotations

--- 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/main.ts
+++ 06-JavascriptAndTypscriptPrimer_Part2/JavascriptPrimer/src/main.ts
@@ -1,6 +1,6 @@
 import { Name, Weather } from "./modules/NameAndWeather";
 import { Name as OtherName } from "./modules/DuplicatedName";
-
+import { TempConverter } from "./modules/TempConverter";
 let name = new Name("Adam", "Smith");
 let weather = new Weather("sunny", "London");
 
@@ -9,3 +9,5 @@ let otherName = new OtherName();
 console.log(name.message);
 console.log(weather.message);
 console.log(otherName.message);
+
+console.log(TempConverter.convertFtoC(38));
