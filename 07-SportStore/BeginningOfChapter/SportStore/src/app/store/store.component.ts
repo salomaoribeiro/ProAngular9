2afa89c0e715d09c9cfa28774c11f8c521526af9
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-24 06:00:03 -0300

    chapter 7 - adding product pagination

--- 07-SportStore/BeginningOfChapter/SportStore/src/app/store/store.component.ts
+++ 07-SportStore/BeginningOfChapter/SportStore/src/app/store/store.component.ts
@@ -8,11 +8,15 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent {
   public selectedCategory = null;
+  public productPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage -1) * this.productPerPage;
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productPerPage);
   }
 
   get categories(): string[] {
@@ -20,4 +24,17 @@ export class StoreComponent {
   }
 
   changeCategory(newCategory?: string) { this.selectedCategory = newCategory; }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productPerPage)).fill(0).map((x, i) => i + 1)
+  }
 }
