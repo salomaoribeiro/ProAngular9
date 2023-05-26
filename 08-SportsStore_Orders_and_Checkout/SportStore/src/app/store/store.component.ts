15b98b09f4574b57bdcacc04864f9efb3aa6a058
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-26 12:47:25 -0300

    chapter 8 - refactoring

---
+++ 08-SportsStore_Orders_and_Checkout/SportStore/src/app/store/store.component.ts
@@ -0,0 +1,44 @@
+import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
+
+@Component({
+  selector: "store",
+  templateUrl: "store.component.html"
+})
+export class StoreComponent {
+  public selectedCategory = null;
+  public productPerPage = 4;
+  public selectedPage = 1;
+
+  constructor(private repository: ProductRepository) { }
+
+  get products(): Product[] {
+    let pageIndex = (this.selectedPage -1) * this.productPerPage;
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productPerPage);
+  }
+
+  get categories(): string[] {
+    return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) { this.selectedCategory = newCategory; }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageCount(): number {
+    return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productPerPage);
+  }
+
+  //get pageNumbers(): number[] {
+  //  return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productPerPage)).fill(0).map((x, i) => i + 1)
+  //}
+}
