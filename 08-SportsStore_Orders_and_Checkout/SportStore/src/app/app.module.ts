cbc6453b0b0fed30c826c3b15abf2c835b18478a
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-27 08:42:34 -0300

    chapter 8 - creating and applying the routing configuration

--- 08-SportsStore_Orders_and_Checkout/SportStore/src/app/app.module.ts
+++ 08-SportsStore_Orders_and_Checkout/SportStore/src/app/app.module.ts
@@ -3,13 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from "./store/store.module";
+import { StoreComponent } from "./store/store.component";
+import { CheckoutComponent } from "./store/checkout.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, StoreModule
+    BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "**", redirectTo: "/store" }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
