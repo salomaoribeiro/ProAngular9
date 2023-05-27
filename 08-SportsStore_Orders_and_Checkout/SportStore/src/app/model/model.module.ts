ad8cfc097dabefb6a3317fc8e5037275e80ecc92
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-27 18:35:05 -0300

    chapter 8 - applying the data source

--- 08-SportsStore_Orders_and_Checkout/SportStore/src/app/model/model.module.ts
+++ 08-SportsStore_Orders_and_Checkout/SportStore/src/app/model/model.module.ts
@@ -4,8 +4,12 @@ import { StaticDataSource } from "./static.datasource";
 import { Cart } from "../model/cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
+import { RestDataSource } from "./rest.datasource";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
+  imports: [HttpClientModule],
+  providers: [ProductRepository, Cart, Order, OrderRepository,
+    { provide: StaticDataSource, useClass: RestDataSource }]
 })
 export class ModelModule { }
