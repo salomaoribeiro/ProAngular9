4c19039ba0a7df6431d95fe68c405e29dc738e19
Salomão Ribeiro <salomaomdm@gmail.com>
2023-05-31 08:59:57 -0300

    chapter 9 - enabling authentication

--- 09-SportsStore_Admin/SportStore/src/app/model/auth.service.ts
+++ 09-SportsStore_Admin/SportStore/src/app/model/auth.service.ts
@@ -8,14 +8,14 @@ export class AuthService {
   constructor(private dataSource: RestDataSource) { }
 
   authenticate(username: string, password: string): Observable<boolean> {
-    return this.datasource.authenticate(username, password);
+    return this.dataSource.authenticate(username, password);
   }
 
   get authenticated(): boolean {
-    return this.datasource.auth_token != null;
+    return this.dataSource.auth_token != null;
   }
 
   clear() {
-    this.datasource.auth_token = null;
+    this.dataSource.auth_token = null;
   }
 }
